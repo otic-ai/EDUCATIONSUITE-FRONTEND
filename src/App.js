41e00c45a300ea5ef4a0e50adf0a732c5e3efaa1
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2023-07-31 08:24:02 +0300

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {Route, Routes, Navigate} from "react-router-dom";
+import React from 'react';
+import Landing from './components/Landing';
+import Billing from './components/Billing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Landing />}/>
+        <Route path="/billing" element={<Billing />}/>
+      </Routes>
     </div>
   );
 }
