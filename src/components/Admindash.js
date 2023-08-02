f02830c680b14aac1ac8a44fa394dc9d271dafcf
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2023-08-02 20:28:32 +0300

    frontend

--- src/components/Admindash.js
+++ src/components/Admindash.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import logo from './images/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faBookReader, faChalkboardTeacher, faClipboardList, faCode, faCog, faFileAlt, faHospital, faHotel, faTable, faUserGraduate} from '@fortawesome/free-solid-svg-icons'
 
 function Admindash() {
   return (
-    <div>Admindash</div>
+    <div className='admin'>
+      <nav>
+        <img src={logo}/>
+        <button>< FontAwesomeIcon className="menu" icon={faBars}/></button>
+        <form>
+          <input type='text' placeholder='Search here' name='name'/>
+        </form>
+      </nav>
+      <div className='sidebar'>
+        <button>Dashboard</button>
+        <ul className='content'>
+          <li>< FontAwesomeIcon className="menu" icon={faUserGraduate}/>Students</li>
+          <li>< FontAwesomeIcon className="menu" icon={faChalkboardTeacher}/>Teachers</li>
+          <li>< FontAwesomeIcon className="menu" icon={faHotel}/>Departments</li>
+          <li>< FontAwesomeIcon className="menu" icon={faBookReader}/>Subjects</li>
+        </ul>
+        <button>Management</button>
+        <ul className='content'>
+          <li>< FontAwesomeIcon className="menu" icon={faFileAlt}/>Accounts</li>
+          <li>< FontAwesomeIcon className="menu" icon={faClipboardList}/>Exam List</li>
+          <li>< FontAwesomeIcon className="menu" icon={faTable}/>Timetable</li>
+          <li>< FontAwesomeIcon className="menu" icon={faCog}/>Settings</li>
+        </ul>
+      </div>
+      <div className='tabs'>
+        <p>Students</p>
+        <p>Awards</p>
+        <p>Department</p>
+        <p>Revenue</p>
+      </div>
+      <div className='tabs1'>
+        <div className='tabs12'>
+        <p>Overview</p>
+        <p>Number of students</p>
+        </div>
+        
+        <div className='tabs12'>
+        <p>Star students</p>
+        <p>Star students</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
