5cb8756c525c4a6abfbe3ee61d11df7f3830ee53
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2023-08-10 20:44:55 +0300

    extra

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,36 +1,49 @@
-import React ,{useContext }from 'react'
-import './Sidebar.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAward, faBars, faBell,faReceipt, faSignOut,faBookReader, faChalkboardTeacher, faClipboardList, faCog, faFileAlt, faGraduationCap, faHome, faHotel, faMoneyBill, faShieldAlt, faTable, faUserFriends, faVectorSquare } from '@fortawesome/free-solid-svg-icons'
-import AuthContext from "../../utils/AuthContext";
-
-
-export default function Sidebar({open,onContentClick}) {
-  let {logoutUser, proxy} = useContext(AuthContext);
+import React from "react";
+import "./Sidebar.css";
+import { dashboarddata, management, pages } from "../../Data/dashboard";
+const Sidebar = () => {
+  console.log(dashboarddata);
   return (
-    <div className={open?"sidebar collapse":"sidebar"}>
-      <button>Dashboard</button>
-      <ul className='content'>
-      <li onClick={() => onContentClick('home')}>< FontAwesomeIcon className="menu" icon={faHome}/>Home</li>
-       <li onClick={() => onContentClick('students')}>< FontAwesomeIcon className="menu" icon={faGraduationCap}/>Students</li>
-       <li onClick={() => onContentClick('teachers')}>< FontAwesomeIcon className="menu" icon={faChalkboardTeacher}/>Teachers</li>
-       <li onClick={() => onContentClick('workspace')}>< FontAwesomeIcon className="menu" icon={faHotel}/>Workspace</li>
-       <li onClick={() => onContentClick('subjects')}>< FontAwesomeIcon className="menu" icon={faBookReader}/>Subjects</li>
-      </ul>
-      <button>Management</button>
-      <ul className='content'>
-        <li>< FontAwesomeIcon className="menu" icon={faFileAlt}/>Accounts</li>
-        <li>< FontAwesomeIcon className="menu" icon={faClipboardList}/> Exam lis</li>
-        <li>< FontAwesomeIcon className="menu" icon={faTable}/>Time Table</li>
-        <li>< FontAwesomeIcon className="menu" icon={faCog}/> Settings</li>
-      </ul>
-      <button>Pages</button>
-      <ul className='content'>
-      <li>< FontAwesomeIcon className="menu" icon={faShieldAlt}/>Authentication</li>
-      <li onClick={() => logoutUser()} >< FontAwesomeIcon className="menu" icon={faSignOut}/>Sign Out</li>
-      </ul>
-        
+    <div className="sidebar">
+      <h1>Dashboard</h1>
+      <div className="sidebar_">
+        {dashboarddata.map((item) => {
+          return (
+            <div className="dashboarddata">
+              <span>{item.icon} </span>
+              <h4>{item.name}</h4>
+              <span>{item.icon1}</span>
+            </div>
+          );
+        })}
+      </div>
+      <h1>Management</h1>
 
+      <div>
+        {management.map((item) => {
+          return (
+            <div className="dashboarddata">
+              <span>{item.icon} </span>
+              <h4>{item.name}</h4>
+              <span>{item.icon1}</span>
+            </div>
+          );
+        })}
+      </div>
+      <h1>Pages</h1>
+      <div>
+        {pages.map((item) => {
+          return (
+            <div className="dashboarddata">
+              <span>{item.icon} </span>
+              <h4>{item.name}</h4>
+              <span>{item.icon1}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Sidebar;
