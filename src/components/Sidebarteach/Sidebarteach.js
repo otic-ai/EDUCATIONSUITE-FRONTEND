925b9df7f550c0f5a9110b783fd0f1c0fc4f77d4
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2023-08-12 14:14:28 +0300

    teacherhome

---
+++ src/components/Sidebarteach/Sidebarteach.js
@@ -0,0 +1,36 @@
+import React ,{useContext }from 'react'
+import './Sidebarteach.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAward, faBars, faBell,faReceipt, faSignOut,faBookReader, faChalkboardTeacher, faClipboardList, faCog, faFileAlt, faGraduationCap, faHome, faHotel, faMoneyBill, faShieldAlt, faTable, faUserFriends, faVectorSquare } from '@fortawesome/free-solid-svg-icons'
+import AuthContext from "../../utils/AuthContext";
+
+
+export default function Sidebarteach({open,onContentClick}) {
+  let {logoutUser, proxy} = useContext(AuthContext);
+  return (
+    <div className={open?"sidebar collapse":"sidebar"}>
+      <button>Dashboard</button>
+      <ul className='content'>
+      <li onClick={() => onContentClick('home2')}>< FontAwesomeIcon className="menu" icon={faHome}/>Home</li>
+       <li onClick={() => onContentClick('students')}>< FontAwesomeIcon className="menu" icon={faGraduationCap}/>Students</li>
+       <li onClick={() => onContentClick('teachers')}>< FontAwesomeIcon className="menu" icon={faChalkboardTeacher}/>Teachers</li>
+       <li onClick={() => onContentClick('workspace')}>< FontAwesomeIcon className="menu" icon={faHotel}/>Workspace</li>
+       <li onClick={() => onContentClick('subjects')}>< FontAwesomeIcon className="menu" icon={faBookReader}/>Subjects</li>
+      </ul>
+      <button>Management</button>
+      <ul className='content'>
+        <li>< FontAwesomeIcon className="menu" icon={faFileAlt}/>Accounts</li>
+        <li>< FontAwesomeIcon className="menu" icon={faClipboardList}/> Exam lis</li>
+        <li onClick={() => onContentClick('timetable')}>< FontAwesomeIcon className="menu" icon={faTable}/>Time Table</li>
+        <li>< FontAwesomeIcon className="menu" icon={faCog}/> Settings</li>
+      </ul>
+      <button>Pages</button>
+      <ul className='content'>
+      <li>< FontAwesomeIcon className="menu" icon={faShieldAlt}/>Authentication</li>
+      <li onClick={() => logoutUser()} >< FontAwesomeIcon className="menu" icon={faSignOut}/>Sign Out</li>
+      </ul>
+        
+
+    </div>
+  )
+}
--- src/components/Teacher/Teachercontent.js
+++ src/components/Teacher/Teachercontent.js
@@ -1,154 +1,29 @@
 import React from "react";
 import { useState } from "react";
 import "./Teachercontent.css";
-import { lessons } from "../../Data/lessons";
-import { piedata } from "../../Data/piechart";
-import { PieChart } from "react-minimal-pie-chart";
-import { history } from "../../Data/history";
-import { events } from "../../Data/upcomingevents";
-import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import Contentanalysis from "../contentanalysis/Contentanalysis";
 import Headerdash from "../Headerdash/Header";
+import Sidebarteach from "../Sidebarteach/Sidebarteach";
+import Teacherhome from "../Teacherhome/Teacherhome";
 
 const Teachercontent = () => {
-  const [data, setdata] = useState(piedata);
-  // clender
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const [selectedContent, setSelectedContent] = useState('home2'); // Default to 'home' content
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
+  const handleclick = () => {
+    setOpen(!open);
+  };
+
+  const handleContentClick = (content) => {
+    setSelectedContent(content);
   };
   return (
     <div className="content">
-    <Headerdash />
+      <Headerdash click={handleclick} />
+      <Sidebarteach onContentClick={handleContentClick} open={open} />
       <Contentanalysis />
-      <div className="content_subsection">
-        <div className="twosection">
-          <div className="uppersection">
-            <div className="lessons">
-              <div>
-                <span>Upcominglesson</span>
-                <span>View all Courses</span>
-              </div>
-              <hr />
-              <div>
-                {lessons.map((item, id) => {
-                  return (
-                    <div key={id} className="scheduleform">
-                      <div className="leftside">
-                        <h4>{item.name}</h4>
-                        <p>{item.desc}</p>
-                        <div className="schedulelist">
-                          <li>{item.dateicon} </li>
-                          <li>{item.date} </li>
-                          <span className="separator">|</span>
-                          <li>{item.timeicon} </li>
-                          <li>{item.time} </li>
-                        </div>
-                      </div>
-                      <div>
-                        <p>{item.status}</p>
-                        <button className="teacher_buttons">
-                          {item.option}
-                        </button>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-            <div className="piecahart">
-              <h3>Teaching Trend</h3>
-              <hr />
-              <p>Lessons Coverage</p>
-              <PieChart
-                data={data}
-                label={({ dataEntry }) =>
-                  `${dataEntry.title}: ${Math.round(dataEntry.percentage)}%`
-                }
-                style={{ width: "150px", height: "150px" }}
-                labelStyle={{
-                  color: "blue",
-                  fontSize: 10,
-                  fontWeight: "bold",
-                }}
-              />
-            </div>
-          </div>
-          <div className="teaching_history">
-            <header className="headers">
-              <p>Teaching History</p>
-              <p>logo</p>
-            </header>
-            <hr />
-            <section className="history_lessons">
-              {history.map((item, id) => {
-                return (
-                  <div key={id} className="scheduleform">
-                    <div className="leftside">
-                      <h4>{item.name}</h4>
-                      <p>{item.desc}</p>
-                      <div className="schedulelist">
-                        <li>{item.dateicon} </li>
-                        <li>{item.date} </li>
-                        <span className="separator">|</span>
-                        <li>{item.timeicon} </li>
-                        <li>{item.time} </li>
-                      </div>
-                    </div>
-                    <div>
-                      <button
-                        className="teacher_buttons"
-                        style={{
-                          backgroundColor:
-                            item.status === "Pending" ? "blue" : "green",
-                        }}>
-                        {item.status}
-                      </button>
-                    </div>
-                  </div>
-                );
-              })}
-            </section>
-          </div>
-        </div>
-
-        <div>
-          <section id="calender">
-            <Calendar onChange={handleDateChange} value={selectedDate} />
-          </section>
-          <section id="upcomingevents">
-            <h3>upcomingevents</h3>
-            <header className="headers">
-              <p>16 june</p>
-              <p>logo</p>
-            </header>
-            <hr />
-            <main>
-              {events.map((item, id) => {
-                return (
-                  <div key={id} className="scheduleform">
-                    <div className="leftside">
-                      <h4>{item.name}</h4>
-                      <p>{item.desc}</p>
-                      <div className="schedulelist">
-                        <li>{item.dateicon} </li>
-                        <li>{item.date} </li>
-                        <li>{item.timeicon} </li>
-                        <li>{item.time} </li>
-                      </div>
-                    </div>
-                    <div>
-                      <p>{item.range}</p>
-                    </div>
-                  </div>
-                );
-              })}
-            </main>
-          </section>
-        </div>
-      </div>
+      {selectedContent === 'home2' && <Teacherhome />}
     </div>
   );
 };
