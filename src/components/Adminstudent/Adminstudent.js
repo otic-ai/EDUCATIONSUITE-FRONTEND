0dea73181902147b90d0e2fc7de04b59d554c274
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2023-08-15 23:13:37 +0300

    students

--- src/components/Adminstudent/Adminstudent.js
+++ src/components/Adminstudent/Adminstudent.js
@@ -1,42 +1,72 @@
-import React from 'react'
-import './Adminstudent.css'
+import React, { useEffect,useState } from 'react';
+import './Adminstudent.css';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 const Adminstudent = () => {
+  const [data,setData] = useState([]);
+  useEffect(() => {
+    axios.get(" http://localhost:3030/students")
+    .then(res => setData(res.data))
+    .catch(err => console.log(err))
+  },[])
+  
+    const [inputData, setInputData] = useState({
+      id:'',
+      name:'',
+      regno:'',
+      class:''
+    });
+    const navigate = useNavigate()
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      axios.post("http://localhost:3030/students", inputData)
+      .then(res => {
+        alert("Student Registered successfully!")
+        navigate('/')
+      })
+    }
   return (
     <div className='payments'>
-    Adminstudent
+    <form onSubmit={handleSubmit}>
+      <div>
+      <label>ID</label>
+      <input type='number' name='id' onChange={e => setInputData({...inputData, id:e.target.value})}/>
+      </div>
+      <div>
+      <label>Name</label>
+      <input type='text' name="name" onChange={e => setInputData({...inputData, name:e.target.value})}/>
+      </div>
+      <div>
+      <label>Reg.no</label>
+      <input type='text' name='regno' onChange={e => setInputData({...inputData, regno:e.target.value})}/>
+      </div>
+      <div>
+      <label>class</label>
+      <input type='text' name='class' onChange={e => setInputData({...inputData, class:e.target.value})}/>
+      </div>
+      <button>Submit</button>
+    </form>
     <table>
         <tr>
           <th>ID</th>
           <th>Name</th>
           <th>Reg.no</th>
           <th>class</th>
+          <th>Action</th>
         </tr>
-      
-          <tr>
-            <td>1</td>
-            <td>Lubwama Eric</td>
-            <td>21/u/12</td>
-            <td>S1</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Rusoke Marvin</td>
-            <td>21/u/15</td>
-            <td>S2</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>SSentamu joseph</td>
-            <td>21/u/127</td>
-            <td>S2</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Mercy Johnson</td>
-            <td>21/u/129</td>
-            <td>S3</td>
-          </tr>
+      {data.map((d,i) => (
+          <tr key={i}>
+          <td>{d.id}</td>
+          <td>{d.name}</td>
+          <td>{d.regno}</td>
+          <td>{d.class}</td>
+          <td>
+            <button>Update</button>
+            <button>Delete</button>
+          </td>
+        </tr>
+      ))}
       </table>
     </div>
   )
